2ba39185be7e3fe1b39499f730bde2dc59ccd40b
kgh128 <hayoung0627@gmail.com>
2023-01-20 18:04:09 +0900

    Solve 2751

---
+++ src/Baekjoon/Sorting/p2751.java
@@ -0,0 +1,30 @@
+package Baekjoon.Sorting;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class p2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder bw = new StringBuilder();
+
+        // 1. 입력받기
+        int N = Integer.parseInt(br.readLine());
+        int[] num = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+        }
+
+        // 2. 오름차순으로 정렬하기
+        Arrays.sort(num);
+
+        // 3. 결과 출력
+        for (int i = 0; i < N; i++) {
+            bw.append(num[i]).append('\n');
+        }
+        System.out.print(bw);
+    }
+}
