5cf4066edabbd9aad3815a8a1d321143acee9e17
김하영 <76698276+kgh128@users.noreply.github.com>
2023-01-29 22:28:35 +0900

    Solve 완주하지 못한 선수

---
+++ src/Programmers/완주하지_못한_선수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<String, Integer>(99999);
+        
+        for (int i = 0; i < completion.length; i++) {
+            if (!map.containsKey(completion[i])) {
+                map.put(completion[i], 1);
+            }
+            else {
+                map.put(completion[i], map.get(completion[i]) + 1);
+            }
+        }
+        
+        for (int i = 0; i < participant.length; i++) {
+            if (!map.containsKey(participant[i]) || map.get(participant[i]) == 0) {
+                answer = participant[i];
+                break;
+            }
+            
+            map.put(participant[i], map.get(participant[i]) - 1);
+        }
+        
+        return answer;
+    }
+}
