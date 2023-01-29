09fe08c4e15cc2e9bc3c3ebe302c583529b05db9
김하영 <76698276+kgh128@users.noreply.github.com>
2023-01-29 22:24:23 +0900

    Solve 폰켓몬

---
+++ src/Programmers/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int getPokemonNums = nums.length / 2;  
+        HashMap<Integer, Integer> pokemonType = new HashMap<Integer, Integer>(10000);
+        
+        for (int i = 0; i < nums.length; i++) {
+            pokemonType.put(nums[i], i);
+        }
+        
+        answer = Math.min(getPokemonNums, pokemonType.size());
+        
+        return answer;
+    }
+}
