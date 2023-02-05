1ba51e726761be6a42d8aca29a0cbfe985adcfdc
kgh128 <hykim359@gmail.com>
2023-02-05 13:56:19 +0900

    Solve K번째수

---
+++ src/Programmers/K번째수.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class K번째수 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] subArray = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(subArray);
+            answer[i] = subArray[commands[i][2]-1];
+        }
+
+        return answer;
+    }
+}
