c0aaa2837c2226feebcb9ce6369501497fdb3a1a
김하영 <76698276+kgh128@users.noreply.github.com>
2023-10-27 17:49:02 +0900

    Delete LeetCode directory

--- LeetCode/28_Find_the_Index_of_the_First_Occurrence_in_a_String.go
+++
@@ -1,5 +0,0 @@
-import "strings"
-
-func strStr(haystack string, needle string) int {
-    return strings.Index(haystack, needle)
-}
--- LeetCode/58_Length_of_Last_Word.go
+++
@@ -1,6 +0,0 @@
-import "strings"
-
-func lengthOfLastWord(s string) int {
-    field := strings.Fields(s)
-    return len(field[len(field)-1])
-}
