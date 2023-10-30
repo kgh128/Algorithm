378db76f5659d7840cdd508ce7f38c106d48d0a3
김하영 <76698276+kgh128@users.noreply.github.com>
2023-10-30 13:36:32 +0900

    Solve 160_Intersection_of_Two_Linked_Lists.go
    - Two-Pointer로 풀었다.

--- src/LeetCode/160_Intersection_of_Two_Linked_Lists.go
+++ src/LeetCode/160_Intersection_of_Two_Linked_Lists.go
@@ -38,5 +38,36 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
 
 /*
 2. Two-Pointer 이용
-- 시간복잡도: O(), 공간복잡도: O(1)
+- 시간복잡도: O(m+n), 공간복잡도: O(1)
+- 이 문제에서 쟁점은 교집합 나오기 전의 링크드 리스트 A와 B의 노드의 개수가 다르다는 것이다.
+- 그래서 하나의 포인터가 하나의 링크드 리스트만 한 번 도는 것으로는 문제를 해결할 수 없다.
+- 따라서 ptr1과 ptr2가 각각 headA와 headB로 시작하고, 각 링크드 리스트를 다 돌았을 경우에는 자신이 돌지 않은 링크드 리스트를 처음부터 돈다.
+- 이 와중에 ptr1과 ptr2가 같으면 교집합 노드를 찾은 것이므로 해당 노드를 리턴한다.
+ex) A = a1 -> a2 -> c1 -> c2
+    B = b1 -> b2 -> b3 -> c1 -> c2
+    ptr1의 순회: a1 -> a2 -> c1 -> c2 -> b1 -> b2 -> b3 -> c1 -> c2
+    ptr2의 순회: b1 -> b2 -> b3 -> c1 -> c2 -> a1 -> a2 -> c1 -> c2
+    이처럼 처음 c1에 도달했을 때는 서로의 시점이 다르지만, 두번째 c1에 도달했을 때는 서로의 시점이 같다.
+- A의 교집합 전의 노드 개수를 a, B의 교집합 전의 노드 개수를 b, 교집합 노드 개수를 c라고 하면
+- 두 포인터 모두 (a + b + c)의 개수만큼 루프를 돌고 같은 시점에 교집합 노드에 도달한다.
 */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+    ptr1 := headA
+    ptr2 := headB
+
+    for ptr1 != ptr2 {
+        if ptr1 == nil {
+            ptr1 = headB
+        } else {
+            ptr1 = ptr1.Next
+        }
+
+        if ptr2 == nil {
+            ptr2 = headA
+        } else {
+            ptr2 = ptr2.Next
+        }
+    }
+
+    return ptr1
+}
